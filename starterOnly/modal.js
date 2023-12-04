d951d6fb2ff3b9a12d37f71368f48056dcbc7db2
angieleutard <leutardangie@MacBook-Pro-de-leutard.local>
2023-12-04 23:16:43 +0100

    add changes

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -85,7 +85,7 @@ let emailVerif = ""
 
 inputEmail.addEventListener("change", (e) => {
   inputEmailValue = e.target.value;
-  if (inputEmailValue.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+  if (inputEmailValue.match(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm)) {
     EmailError.style.display = "none";
     inputEmail.classList.add("valid-text-control");
     emailVerif = true
